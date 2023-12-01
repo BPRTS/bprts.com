9343eff8cf5aec22d4c5a8d42ed966ec02bbe9df
BPRTS <bas@bprts.com>
2023-12-01 13:56:07 +0100

    bruh

--- app.js
+++ app.js
@@ -7,13 +7,14 @@ class App {
         this.canvas = document.createElement('canvas');
         this.ctx = this.canvas.getContext('2d');
         document.body.appendChild(this.canvas);
-
         this.WaveGroup = new WaveGroup();
-;
+
         window.addEventListener('resize', this.resize.bind(this), false);
         this.resize();
 
         requestAnimationFrame(this.animate.bind(this));
+
+        
     }
 
     resize() {
