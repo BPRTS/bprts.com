59a33f5669f3749b315fbf4e5bc7d91bf96660a1
BPRTS <bas@bprts.com>
2023-12-02 00:12:58 +0100

    that took loongg

--- wavegroup.js
+++ wavegroup.js
@@ -7,8 +7,9 @@ export class WaveGroup {
         this.totalWaves = 3;
         this.totalPoints = 6;
 
-        this.color = ['rgba(0,199,235,0.4)', 'rgba(0,146,199,0.4)', 'rgba(0,87,158,0.4)'];
-
+        //this.color = ['rgba(0,199,235,0.4)', 'rgba(0,146,199,0.4)', 'rgba(0,87,158,0.4)'];
+        this.color = ['rgba(255,255,255,1)', 'rgba(255,255,255,0.4)', 'rgba(255,255,255,0.4)'];
+        
         this.waves = [];
 
         for (let i = 0; i < this.totalWaves; i++) {
