e698edee34ac48e76effbb47c4c7dcf26e420b6f
BPRTS <bas@bprts.com>
2024-02-29 17:11:09 +0100

    start of connecting databases

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "2182";
+$dbName = "Projects";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
